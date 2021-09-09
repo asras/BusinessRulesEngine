29057b433471f5602a4f3247d1c4d2a66090fc30
asras <asras28@gmail.com>
2021-09-09 11:12:50 +0200

    Added action data test

---
+++ BusinessRulesEngine/Tests/TestActionData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace Tests
+{
+    [TestClass]
+    public class TestActionData
+    {
+        [TestMethod]
+        public void EqualityIsByValue()
+        {
+            var success = true;
+            var desc = "A description";
+            var id = Guid.NewGuid();
+
+            var data1 = new ActionData(success, desc, id);
+            var data2 = new ActionData(success, desc, id);
+
+            Assert.AreEqual(data1, data2);
+
+        }
+    }
+}
